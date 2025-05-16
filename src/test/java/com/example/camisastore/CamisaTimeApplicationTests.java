8c73b5bef9ae640603f45d161526924b1d13f5fb
Lucas <lucasnicos2016@gmail.com>
2025-05-16 15:06:44 -0300

    Add files via upload

---
+++ src/test/java/com/example/camisastore/CamisaTimeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.camisastore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamisaTimeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
