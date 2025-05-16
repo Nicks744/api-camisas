bc34c0c197916c815f137f2f46827923bd4130d0
Lucas <lucasnicos2016@gmail.com>
2025-05-16 15:06:15 -0300

    Delete camisastore directory

--- camisastore/src/main/java/com/example/camisastore/repository/CamisaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.camisastore.repository;
-
-import com.example.camisastore.model.Camisa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CamisaRepository extends JpaRepository<Camisa, Long> {
-}
